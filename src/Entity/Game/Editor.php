43f42d621646ecc1a7b77d90c7497352b1996c51
Koretech <oudart.aurelien@live.fr>
2025-07-23 22:27:54 +0200

    Ajoute l'entité Game/Editor
    - Ajoute le Game/EditorRepository

---
+++ src/Entity/Game/Editor.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Game;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Table;
+
+#[ORM\Entity(readOnly: true)]
+#[Table(name: 'game_editors')]
+class Editor
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(name: 'game_editor_name')]
+    private string $name;
+
+    #[ORM\Column(name: 'is_game_editor_new', type: Types::BOOLEAN)]
+    private bool $isNew;
+}
