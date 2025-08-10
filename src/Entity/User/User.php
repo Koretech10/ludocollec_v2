c05292819f6774bce04d723496b351f2b944c131
Koretech <oudart.aurelien@live.fr>
2025-08-10 18:06:01 +0200

    Implémente les interfaces utilisateurs dans User

--- src/Entity/User/User.php
+++ src/Entity/User/User.php
@@ -12,16 +12,21 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(readOnly: true)]
 #[ORM\Table(name: 'users')]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: Types::INTEGER)]
     private int $id;
 
+    /**
+     * @var non-empty-string
+     */
     #[ORM\Column(unique: true)]
     private string $username;
 
@@ -93,4 +98,34 @@ class User
 
     #[ORM\Column(enumType: DisplayListType::class, options: ['default' => DisplayListType::CARD_GRID])]
     private DisplayListType $displayListType = DisplayListType::CARD_GRID;
+
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+        $data[\sprintf("\0%s\0password", self::class)] = hash('crc32c', $this->password ?? '');
+
+        return $data;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+
+        return \array_unique($roles);
+    }
+
+    #[\Deprecated('to be removed when upgrading to Symfony 8')]
+    public function eraseCredentials(): void
+    {
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->username;
+    }
 }
