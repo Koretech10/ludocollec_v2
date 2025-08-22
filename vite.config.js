49d31ab9a106fae24b8f2ae04404dd514a581253
Aurélien OUDART <aoudart@hexanet.fr>
2025-08-22 21:27:26 +0200

    Ignore les dépréciations de Bootstrap

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,11 @@ export default defineConfig({
             },
         }
     },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true,
+            },
+        },
+    },
 });
