251fe5400ecfcf5cb5bbecc1565f40df5bb5d65b
Aurélien OUDART <aoudart@hexanet.fr>
2025-08-22 22:29:07 +0200

    Ignore les dépréciations @import
    - Bootstrap et Bootstrap Icons ne fonctionnent pas sans pour l'instant

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,9 @@ export default defineConfig({
         preprocessorOptions: {
             scss: {
                 quietDeps: true,
+                silenceDeprecations: [
+                    'import',
+                ],
             },
         },
     },
