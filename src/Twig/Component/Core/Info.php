0ed04abbff2a7db2fc41318a5e3ceab5c43a6fab
Koretech <oudart.aurelien@live.fr>
2025-09-15 22:16:47 +0200

    Ajoute le composant Info

---
+++ src/Twig/Component/Core/Info.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Component\Core;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent(
+    name: 'info',
+    template: 'component/core/info.html.twig',
+)]
+class Info
+{
+    public string $label;
+    public string $value;
+}
