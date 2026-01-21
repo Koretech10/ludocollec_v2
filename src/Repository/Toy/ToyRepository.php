ad5a9f392e6c153e3283f6456baaa663dfef514b
Aurélien OUDART <aoudart@hexanet.fr>
2026-01-21 13:49:34 +0100

    Ajoute la requête findForListQueryBuilder à ToyRepository

--- src/Repository/Toy/ToyRepository.php
+++ src/Repository/Toy/ToyRepository.php
@@ -7,6 +7,7 @@ namespace App\Repository\Toy;
 use App\Entity\Toy\Toy;
 use App\Repository\BaseActionsTrait;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 class ToyRepository extends ServiceEntityRepository
@@ -17,5 +18,13 @@ class ToyRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Toy::class);
+    }
+
+    public function findForListQueryBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder('toy')
+            ->innerJoin('toy.series', 'series')
+            ->innerJoin('toy.manufacturer', 'manufacturer')
+        ;
     }
 }
