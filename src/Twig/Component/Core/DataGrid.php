d31980c949639f3b4f3abd4a91939ae4dea5f662
Aurélien OUDART <aoudart@hexanet.fr>
2026-02-18 13:30:43 +0100

    Ajoute le composant Twig DataGrid

---
+++ src/Twig/Component/Core/DataGrid.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Component\Core;
+
+use App\Util\DataGrid\DataGrid as DataGridModel;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent(name: 'data-grid')]
+class DataGrid
+{
+    public DataGridModel $dataGrid;
+
+    public function getPager(): PaginationInterface
+    {
+        return $this->dataGrid->pager;
+    }
+}
