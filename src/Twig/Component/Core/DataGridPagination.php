34346854851137e74ee255a02834b85578c40daf
Aurélien OUDART <aoudart@hexanet.fr>
2026-02-23 13:29:08 +0100

    Ajoute le composant DataGridPagination

---
+++ src/Twig/Component/Core/DataGridPagination.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Component\Core;
+
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('data-grid-pagination')]
+class DataGridPagination
+{
+    public PaginationInterface $pager;
+
+    public function getFirstResultNumber(): int
+    {
+        $itemsPerPage = $this->pager->getItemNumberPerPage();
+        $page = $this->pager->getCurrentPageNumber();
+
+        return ($itemsPerPage * $page) - ($itemsPerPage - 1);
+    }
+
+    public function getLastResultNumber(): int
+    {
+        $count = $this->pager->count();
+
+        return $this->getFirstResultNumber() + $count - 1;
+    }
+
+    public function getTotalItemCount(): int
+    {
+        return $this->pager->getTotalItemCount();
+    }
+}
