bae7c1030fac52e843dbb399c8909895db25237e
Aurélien OUDART <aoudart@hexanet.fr>
2026-06-06 22:33:36 +0200

    Déplace LoginType

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Form\LoginType;
+use App\Form\Core\LoginType;
 use SlopeIt\BreadcrumbBundle\Attribute\Breadcrumb;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
