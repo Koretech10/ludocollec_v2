3ad718050ca9856ae5f74b474414492263667fa9
Aurélien OUDART <aoudart@hexanet.fr>
2026-06-07 22:11:09 +0200

    Corrige la pagination

--- src/Twig/Component/Core/DataGridPagination.php
+++ src/Twig/Component/Core/DataGridPagination.php
@@ -33,6 +33,10 @@ class DataGridPagination
 
     public function getFirstResultNumber(): int
     {
+        if (0 === $this->getTotalItemCount()) {
+            return 0;
+        }
+
         $itemsPerPage = $this->pager->getItemNumberPerPage();
         $page = $this->pager->getCurrentPageNumber();
 
@@ -41,6 +45,10 @@ class DataGridPagination
 
     public function getLastResultNumber(): int
     {
+        if (0 === $this->getTotalItemCount()) {
+            return 0;
+        }
+
         $count = $this->pager->count();
 
         return $this->getFirstResultNumber() + $count - 1;
