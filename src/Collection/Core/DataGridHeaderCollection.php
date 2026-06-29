6b0a57973f1616f3f329abbf0e5a6ef2cbf65389
Aurélien OUDART <aoudart@hexanet.fr>
2026-06-29 13:20:53 +0200

    Nettoie la DataGridHeaderCollection

--- src/Collection/Core/DataGridHeaderCollection.php
+++ src/Collection/Core/DataGridHeaderCollection.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Collection\Core;
 
-use App\Exception\Core\DataGridHeaderNotFoundException;
 use App\Model\Core\DataGrid\DataGridHeader;
 use Doctrine\Common\Collections\ArrayCollection;
 
@@ -13,27 +12,4 @@ use Doctrine\Common\Collections\ArrayCollection;
  */
 class DataGridHeaderCollection extends ArrayCollection
 {
-    public function getHeadersWithKey(): self
-    {
-        return $this->filter(static fn (DataGridHeader $header): bool => null !== $header->key);
-    }
-
-    /**
-     * @throws DataGridHeaderNotFoundException
-     */
-    public function getForKey(string $key): DataGridHeader
-    {
-        /** @var DataGridHeader|false $header */
-        $header = $this->filter(
-            static function (DataGridHeader $header) use ($key): bool {
-                return $header->key === $key;
-            }
-        )->first();
-
-        if (false === $header) {
-            throw DataGridHeaderNotFoundException::forKey($key);
-        }
-
-        return $header;
-    }
 }
