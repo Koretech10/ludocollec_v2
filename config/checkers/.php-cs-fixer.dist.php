028a6f3ee25aee57644308d2fb933ee883b11957
Aurélien OUDART <aoudart@hexanet.fr>
2026-07-27 13:56:43 +0200

    Corrige PHP CS Fixer

--- config/checkers/.php-cs-fixer.dist.php
+++ config/checkers/.php-cs-fixer.dist.php
@@ -10,11 +10,13 @@ return new PhpCsFixer\Config()
         '@Symfony' => true,
         'array_syntax' => ['syntax' => 'short'],
         'no_unused_imports' => true,
+        'no_useless_else' => true,
         'ordered_imports' => true,
         'phpdoc_order' => true,
         'align_multiline_comment' => true,
         'compact_nullable_type_declaration' => true,
         'nullable_type_declaration_for_default_null_value' => true,
+        'phpdoc_to_comment' => ['ignored_tags' => ['var', 'phpstan-ignore']],
     ])
     ->setFinder($finder)
 ;
