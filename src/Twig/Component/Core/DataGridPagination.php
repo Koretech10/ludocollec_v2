21d8ef669aca102e7d80b2a9f56ebd202aec0cf4
Aurélien OUDART <aoudart@hexanet.fr>
2026-07-27 13:41:26 +0200

    Modifie le comportement de DataGridPagination
    - Le selectedDisplayListType doit être donné par le DataGrid

--- src/Twig/Component/Core/DataGridPagination.php
+++ src/Twig/Component/Core/DataGridPagination.php
@@ -5,34 +5,19 @@ declare(strict_types=1);
 namespace App\Twig\Component\Core;
 
 use App\Collection\Core\DataGridHeaderCollection;
-use App\Cookie\DisplayListTypeCookie;
 use App\Enum\User\DisplayListType;
 use Knp\Component\Pager\Pagination\PaginationInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
-use Symfony\UX\TwigComponent\Attribute\PostMount;
 
 #[AsTwigComponent('data-grid-pagination')]
 class DataGridPagination
 {
-    private DisplayListType $selectedDisplayListType;
-
+    public DisplayListType $selectedDisplayListType;
     public PaginationInterface $pager;
     public string $filterModalId;
     public bool $hasFilterType;
     public bool $isFiltered;
     public DataGridHeaderCollection $headers;
-
-    public function __construct(
-        private readonly RequestStack $requestStack,
-    ) {
-    }
-
-    #[PostMount]
-    public function postMount(): void
-    {
-        $this->setSelectedDisplayListType();
-    }
 
     public function getFirstResultNumber(): int
     {
@@ -60,23 +45,5 @@ class DataGridPagination
     public function getTotalItemCount(): int
     {
         return $this->pager->getTotalItemCount();
-    }
-
-    public function getSelectedDisplayListType(): DisplayListType
-    {
-        return $this->selectedDisplayListType;
-    }
-
-    private function setSelectedDisplayListType(): void
-    {
-        $request = $this->requestStack->getCurrentRequest();
-
-        if (null === $request) {
-            return;
-        }
-
-        $displayListTypeCookie = $request->cookies->getInt(DisplayListTypeCookie::DISPLAY_LIST_COOKIE_NAME);
-
-        $this->selectedDisplayListType = DisplayListType::from($displayListTypeCookie);
     }
 }
