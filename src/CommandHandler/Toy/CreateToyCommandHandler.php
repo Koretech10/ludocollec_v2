b4d0f85b3e6da7ead2dcd6bd1dddfe34a9858c0c
Aurélien OUDART <aoudart@hexanet.fr>
2026-08-06 23:09:22 +0200

    Intègre les images téléversées
    - Ajoute les méthodes processUplaodedFile et resizeImage à ImageManager
    - Ajoute l'exception InvalidMimeType

--- src/CommandHandler/Toy/CreateToyCommandHandler.php
+++ src/CommandHandler/Toy/CreateToyCommandHandler.php
@@ -7,7 +7,10 @@ namespace App\CommandHandler\Toy;
 use App\Command\Toy\CreateToyCommand;
 use App\Entity\Toy\Toy;
 use App\Event\Toy\ToyCreatedEvent;
+use App\Exception\Core\EntityNotManagedException;
+use App\Exception\Core\InvalidMimeTypeException;
 use App\Repository\Toy\ToyRepository;
+use App\Util\Core\ImageManager;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
@@ -17,11 +20,18 @@ readonly class CreateToyCommandHandler
     public function __construct(
         private ToyRepository $toyRepository,
         private EventDispatcherInterface $eventDispatcher,
+        private ImageManager $imageManager,
     ) {
     }
 
+    /**
+     * @throws InvalidMimeTypeException
+     * @throws EntityNotManagedException
+     */
     public function __invoke(CreateToyCommand $command): int
     {
+        $image = $command->image;
+
         $toy = new Toy(
             $command->name,
             $command->manufacturer,
@@ -30,9 +40,11 @@ readonly class CreateToyCommandHandler
             $command->author,
         );
 
-        // ToDo Upload image
-
         $this->toyRepository->persistAndFlush($toy);
+
+        if (null !== $image) {
+            $this->imageManager->processUploadedFile($image, $toy);
+        }
 
         $this->eventDispatcher->dispatch(new ToyCreatedEvent($toy));
 
--- src/Util/Core/ImageManager.php
+++ src/Util/Core/ImageManager.php
@@ -7,7 +7,13 @@ namespace App\Util\Core;
 use App\Entity\ImageableEntity;
 use App\Enum\Core\EntityToImageDir;
 use App\Exception\Core\EntityNotManagedException;
+use App\Exception\Core\InvalidMimeTypeException;
+use Imagine\Gd\Imagine;
+use Imagine\Image\Box;
+use Imagine\Image\ImageInterface;
+use Imagine\Image\ManipulatorInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 readonly class ImageManager
 {
@@ -29,13 +35,54 @@ readonly class ImageManager
      */
     public function getImagePath(ImageableEntity $entity): string
     {
-        $entityImageDir = EntityToImageDir::fromEntityClass($entity::class);
-        $imagePath = \sprintf('%s/%s/%s.jpg', self::IMAGE_ROOT, $entityImageDir->value, $entity->getId());
+        $imagePath = $this->getEntityImageDirPath($entity);
 
         if ($this->filesystem->exists($imagePath)) {
             return \sprintf('/%s', $imagePath);
         }
 
         return \sprintf('/%s', $this->defaultImagePath);
+    }
+
+    /**
+     * @throws InvalidMimeTypeException
+     * @throws EntityNotManagedException
+     */
+    public function processUploadedFile(UploadedFile $uploadedFile, ImageableEntity $entity): void
+    {
+        $mimeType = $uploadedFile->getMimeType();
+
+        if (null === $mimeType || !\str_starts_with($mimeType, 'image/')) {
+            throw InvalidMimeTypeException::expectsImage($mimeType ?? 'NULL');
+        }
+
+        $imagine = new Imagine();
+
+        $image = $imagine->open($uploadedFile->getPathname());
+
+        $resizedImage = $this->resizeImage($image);
+
+        $resizedImage->save($this->getEntityImageDirPath($entity), [
+            'format' => 'jpeg',
+            'jpeg_quality' => 100,
+        ]);
+    }
+
+    /**
+     * @throws EntityNotManagedException
+     */
+    private function getEntityImageDirPath(ImageableEntity $entity): string
+    {
+        $entityImageDir = EntityToImageDir::fromEntityClass($entity::class);
+
+        return \sprintf('%s/%s/%s.jpg', self::IMAGE_ROOT, $entityImageDir->value, $entity->getId());
+    }
+
+    private function resizeImage(ImageInterface $image, int $maxLength = 1080): ManipulatorInterface
+    {
+        $maxDimensions = new Box($maxLength, $maxLength);
+
+        // THUMBNAIL_INSET va adapter l'image aux dimensions maximales définies.
+        return $image->thumbnail($maxDimensions, ManipulatorInterface::THUMBNAIL_INSET);
     }
 }
