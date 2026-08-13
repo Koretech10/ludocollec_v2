59db1b3790a1b619a2f20a8862a51fb8b5b0616a
Aurélien OUDART <aoudart@hexanet.fr>
2026-08-13 21:35:19 +0200

    Ajoute le PreviewableImageType

--- src/Form/Toy/CreateToyType.php
+++ src/Form/Toy/CreateToyType.php
@@ -7,10 +7,10 @@ namespace App\Form\Toy;
 use App\Command\Toy\CreateToyCommand;
 use App\Entity\Toy\Manufacturer;
 use App\Entity\Toy\Series;
+use App\Form\Core\Type\PreviewableImageType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -38,13 +38,8 @@ class CreateToyType extends AbstractType
             'label' => 'Date de première sortie',
         ]);
 
-        $builder->add('image', FileType::class, [
+        $builder->add('image', PreviewableImageType::class, [
             'label' => 'Image du jouet vidéo',
-            'help' => 'Taille maximale : 2 Mo',
-            'required' => false,
-            'attr' => [
-                'data-ludocollec-target' => 'image-input',
-            ],
         ]);
 
         $builder->add('submit', SubmitType::class, [
