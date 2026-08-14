e50111f1464177c0126176d2ca4e1e45d653c8af
Aurélien OUDART <aoudart@hexanet.fr>
2026-08-14 16:37:07 +0200

    Gère les actions

--- src/Twig/Component/Core/DataGrid.php
+++ src/Twig/Component/Core/DataGrid.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace App\Twig\Component\Core;
 
+use App\Collection\Core\DataGridActionCollection;
 use App\Collection\Core\DataGridHeaderCollection;
 use App\Enum\User\DisplayListType;
 use App\Model\Core\DataGrid\DataGrid as DataGridModel;
+use App\Model\Core\DataGrid\DataGridAction;
 use App\Query\Core\GetDisplayListTypeQuery;
 use Knp\Component\Pager\Pagination\PaginationInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Messenger\HandleTrait;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 use Symfony\UX\TwigComponent\Attribute\PostMount;
 
@@ -27,6 +30,7 @@ class DataGrid
     public bool $hideActions = false;
 
     public function __construct(
+        private readonly UrlGeneratorInterface $urlGenerator,
         MessageBusInterface $queryBus,
     ) {
         $this->messageBus = $queryBus;
@@ -69,5 +73,20 @@ class DataGrid
     public function hasFilterType(): bool
     {
         return null !== $this->dataGrid->getFilterType();
+    }
+
+    public function getActions(): DataGridActionCollection
+    {
+        return $this->dataGrid->actions;
+    }
+
+    public function getActionHref(DataGridAction $action, object $item): string
+    {
+        return $this->urlGenerator->generate($action->route, $action->resolveRouteParameters($item));
+    }
+
+    public function canShowAction(DataGridAction $action, object $item): bool
+    {
+        return $action->canShow($item);
     }
 }
