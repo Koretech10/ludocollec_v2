359e9606c398b562bbd427660fe4398761a1568c
Aurélien OUDART <aoudart@hexanet.fr>
2026-08-17 13:10:55 +0200

    Ajoute le composant Dropdown

---
+++ src/Twig/Component/Core/Dropdown.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Component\Core;
+
+use App\Enum\Core\Type;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('dropdown')]
+class Dropdown
+{
+    public Type $type = Type::SECONDARY;
+    public bool $outlined = false;
+}
