5d70e7c5428a2b7f28f987307b474be0ebf32611
Aurélien OUDART <aoudart@hexanet.fr>
2026-08-21 16:09:42 +0200

    Passe les champs en EntityAutocompleteType

--- src/Form/Toy/CreateToyType.php
+++ src/Form/Toy/CreateToyType.php
@@ -7,8 +7,8 @@ namespace App\Form\Toy;
 use App\Command\Toy\CreateToyCommand;
 use App\Entity\Toy\Manufacturer;
 use App\Entity\Toy\Series;
+use App\Form\Core\Type\EntityAutocompleteType;
 use App\Form\Core\Type\PreviewableImageType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -24,12 +24,12 @@ class CreateToyType extends AbstractType
             'label' => 'Nom du jouet vidéo',
         ]);
 
-        $builder->add('series', EntityType::class, [
+        $builder->add('series', EntityAutocompleteType::class, [
             'label' => 'Série',
             'class' => Series::class,
         ]);
 
-        $builder->add('manufacturer', EntityType::class, [
+        $builder->add('manufacturer', EntityAutocompleteType::class, [
             'label' => 'Fabricant',
             'class' => Manufacturer::class,
         ]);
